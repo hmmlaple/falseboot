30e42d4fda4f85b041e5834fe906bb974562ec2f
Hmmlopl <75092363+hmmlaple@users.noreply.github.com>
2022-06-04 00:42:11 +0000

    P A I N

---
+++ false-core/faker.py
@@ -0,0 +1,12 @@
+#redirect to docker container to capture user login
+#imports
+import os
+import sys
+import time
+import random
+import requests
+import json
+import subprocess
+import re
+import docker
+
