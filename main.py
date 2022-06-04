5bfcd0c6b8a5373f251ad58cca83b33e7ca6e498
Hmmlopl <75092363+hmmlaple@users.noreply.github.com>
2022-06-04 05:22:18 +0000

    sussy baka

---
+++ main.py
@@ -0,0 +1,25 @@
+#ignore this file it is a placeholder for the future
+import os   
+import subprocess
+import time
+import sys
+import docker
+import shutil
+#imports done
+#ask for password
+#the password is lopl
+password='lopl'
+#ask for password done
+print("enter password")
+#get password
+password_input=input()
+#get password done
+#check if password is correct
+if password_input==password:
+    print("password correct")
+    #sys.exit(0)
+else:
+    #run startup.py
+    os.system("cd false-core && python3 startup.py")
+    #run startup.py done
+    
