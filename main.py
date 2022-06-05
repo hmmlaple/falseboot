3f3d62a23d9c55a79f6e9c20482bd382e8168df8
Hmmlopl <75092363+hmmlaple@users.noreply.github.com>
2022-06-05 16:15:25 +0000

    whatyuiop[]

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
-#ignore this file it is a placeholder for the future
-import os   
+# ignore this file it is a placeholder for the future
+import os
 import subprocess
 import time
 import sys
 import docker
 import shutil
-#imports done
-#ask for password
-#the password is lopl
-password='lopl'
-#clear screen
+# imports done
+# ask for password
+# clear screen
 os.system('clear')
-#ask for password done
+# ask for password done
 print("enter password")
-#get password
-password_input=input()
-#get password done
-#check if password is correct
-if password_input==password:
+# get password
+password = 'lopl'
+password_input = input()
+# get password done
+# check if password is correct
+if password_input == password:
     print("password correct")
-    #run true-core/startup.py
+    # run true-core/startup.py
     os.system("python3 true-core/startup.py")
-    #sys.exit(0)
+    # sys.exit(0)
 else:
-    #run startup.py
+    # run startup.py
     os.system("cd false-core && python3 startup.py")
-    #run startup.py done
-    
+    # run startup.py done
